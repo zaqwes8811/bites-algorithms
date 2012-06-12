e2545f0f391b45179ff3b2a773e4483b0f4ca427
zaqwes <luganskyia8811@gmail.com>
2012-06-12 14:33:44 +0400

    add

---
+++ trees/Копия main.cpp
@@ -0,0 +1,30 @@
+// color your text in Windows console mode
+// colors are 0=black 1=blue 2=green and so on to 15=white  
+// colorattribute = foreground + background * 16
+// to get red text on yellow use 4 + 14*16 = 228
+// light red on yellow would be 12 + 14*16 = 236
+// a Dev-C++ tested console application by  vegaseat  07nov2004
+ 
+#include <iostream>
+#include <windows.h>   // WinApi header
+ 
+using namespace std;    // std::cout, std::cin
+ 
+int main()
+{
+  HANDLE  hConsole;
+	int k;
+ 
+  hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+ 
+  // you can loop k higher to see more color choices
+  for(k = 1; k < 255; k++)
+  {
+    // pick the colorattribute k you want
+    SetConsoleTextAttribute(hConsole, k);
+    cout << k << " I want to be nice today!" << endl;
+  }
+ 
+  cin.get(); // wait
+  return 0;
+}
