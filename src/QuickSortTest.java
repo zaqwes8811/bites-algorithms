a2ff0b23a1d7e7d28e030c499cc44086cf6bc043
bb <igor.a.lugansky@gmail.com>
2013-09-29 16:51:01 +0400

    Befor reading from file.

--- src/QuickSortTest.java
+++ src/QuickSortTest.java
@@ -1,3 +1,9 @@
+import com.google.common.collect.ImmutableList;
+import com.google.common.io.Closer;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -20,6 +26,7 @@ public class QuickSortTest {
     System.out.println(array);
   }
 
+  @org.junit.Test
   public void testBoundary() throws Exception {
     QuickSort sorter = new QuickSort();
     Integer [] rawArray = {3,8,2,5, 6,4,7,1};
@@ -28,5 +35,26 @@ public class QuickSortTest {
 
     sorter.quickSort(array);
     System.out.println(array);
+  }
+
+  @org.junit.Test
+  public void testFromFile() throws Exception {
+
+  }
+
+  static private ImmutableList<String> fileToList(String filename) throws IOException {
+    Closer closer = Closer.create();
+    List<String> result = new ArrayList<String>();
+    try {
+      // TODO(zaqwes): Может лучше считать разом, а потом разбить на части?
+      BufferedReader in = closer.register(new BufferedReader(new FileReader(filename)));
+      String s;
+      while ((s = in.readLine()) != null) result.add(s);
+    } catch (Throwable e) {
+      closer.rethrow(e);
+    } finally {
+      closer.close();
+    }
+    return ImmutableList.copyOf(result);
   }
 }
