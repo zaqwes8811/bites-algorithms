edefb81e7aade53ec0dfeb68b438997c82d3084e
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-22 17:12:30 +0400

    list<list> -> dict

--- graphs/min_cut.py
+++ graphs/min_cut.py
@@ -1,12 +1,29 @@
 # coding: utf-8
 
+import copy
+import random
 
-def get_random_edge(adj_list):
-    pass
+
+def get_random_edge(find_edge_graph):
+    """
+    Not None in list!
+    """
+    source_v = random.randint(0, len(find_edge_graph)-1)
+    connections = find_edge_graph[source_v]
+    end_v = connections[random.randint(0, len(connections)-1)]
+    return source_v+1, end_v
 
 
 def main():
-    adj_list = [[2, 4], [1, 4, 3], [2, 4], [1, 2, 3]]
+    """
+    Чтобы извлекать за константное время нужно оставлять ссылки на местах.
+    Чтобы рандомно выбирать за конастантное время нужно ужимать список
+    """
+    source_graph = {1:[2, 4], 2:[1, 4, 3], 3:[2, 4], 4:[1, 2, 3]}
+
+    for i in range(19):
+        print get_random_edge(source_graph)
+
 
 
 main()
