8d1c97a457abe71ae845c66a44314bdd6a91381a
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-22 19:51:12 +0400

    Don't understood mincut

--- graphs/min_cut.py
+++ graphs/min_cut.py
@@ -26,7 +26,77 @@ def main():
     source_graph = {1: [2, 4], 2: [1, 4, 3], 3: [2, 4], 4: [1, 2, 3]}
 
     # One iteration
-    source, end = get_random_edge(source_graph)
+    while source_graph.__len__() > 2:
+        edge = get_random_edge(source_graph)
+        fuse(source_graph, edge)
+
+    print source_graph
+
+
+def fuse(graph, edge):
+    # TODO: удалить ребро
+    source = edge[0]
+    end = edge[1]
+
+    # source <- end
+    ends_of_source = graph[source]
+    ends_of_end = graph[end]
+
+    # Дублированные удалять нельзя, но нужно удалять петли
+    ends_of_source.extend(ends_of_end)
+
+    graph[source] = filter(lambda a: a != source, graph[source])
+    del graph[end]
+
+
+def remove_key(d, key):
+    r = dict(d)
+    del r[key]
+    return r
+
+
+def benchmark_delete_key():
+    """
+    http://docs.python.org/2/library/collections.html
+
+    """
+
+    import timeit
+
+    code = """
+    results = {'A': 1, 'B': 2, 'C': 3}
+    del results['A']
+    del results['B']
+    """
+    print timeit.timeit(code, number=100000)
+    code = """
+    results = {'A': 1, 'B': 2, 'C': 3}
+    results.pop('A')
+    results.pop('B')
+    """
+    print timeit.timeit(code, number=100000)
+
+    code = """
+    results = {'A': 1, 'B': 2, 'C': 3}
+    def remove_key(d, key):
+        r = dict(d)
+        del r[key]
+        return r
+    remove_key(results, 'A')
+    remove_key(results, 'B')
+    """
+    print timeit.timeit(code, number=100000)
+
+    code = """
+    #import collections
+    for i in range(100000):
+        #results = collections.defaultdict({'A': 1, 'B': 2, 'C': 3})
+        #del results['A']
+        #del results['B']
+        pass
+    """
+    print timeit.timeit(code, number=10)
 
 
 main()
+#benchmark_delete_key()
