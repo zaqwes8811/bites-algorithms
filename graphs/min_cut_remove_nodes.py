4088f0bce499af03cf2b1aa1b4b733a7d7420e7c
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-23 21:00:33 +0400

    save and push

--- graphs/min_cut_remove_nodes.py
+++ graphs/min_cut_remove_nodes.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 
+import collections
+
 
 def main():
     source_graph = {1: [2, 4], 2: [1, 4, 3], 3: [2, 4], 4: [1, 2, 3]}
@@ -11,5 +13,17 @@ def main():
 
             # Рабочее пложение ребра
             self.work = (begin, end)
+
+        def __str__(self):
+            return str(self.work)
+
+    # BAD! Но это указатели, а не копии объектов. Может не так все печально?
+    work_graph = collections.defaultdict(list)
+    for k, v in source_graph.items():
+        for value in v:
+            work_graph[k].append(Edge(k, value))
+
+    for k, v in work_graph.items():
+        print v
 
 main()
