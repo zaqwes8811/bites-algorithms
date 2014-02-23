12271d448bce531c293281057b8c31acb25f3214
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-23 20:50:46 +0400

    save and push

--- graphs/min_cut_remove_nodes.py
+++ graphs/min_cut_remove_nodes.py
@@ -4,4 +4,12 @@
 def main():
     source_graph = {1: [2, 4], 2: [1, 4, 3], 3: [2, 4], 4: [1, 2, 3]}
 
+    class Edge(object):
+        def __init__(self, begin, end):
+            # Исходное пложение ребра
+            self.source_ = (begin, end)
+
+            # Рабочее пложение ребра
+            self.work = (begin, end)
+
 main()
