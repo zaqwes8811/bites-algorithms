bed790482f5df719e312a21eacd907e8ca83919b
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-02-23 21:54:09 +0400

    Need test fuse.

--- graphs/min_cut_remove_nodes.py
+++ graphs/min_cut_remove_nodes.py
@@ -1,6 +1,31 @@
 # coding: utf-8
 
 import collections
+import copy
+
+
+class Edge(object):
+    def __init__(self, begin, end):
+        # Исходное пложение ребра
+        self.source_ = (begin, end)
+
+        # Рабочее пложение ребра
+        self.work = (begin, end)
+
+    @property
+    def begin(self):
+        return self.work[0]
+
+    @begin.setter
+    def begin(self, value):
+        self.work = value, self.work[1]
+
+    @property
+    def end(self):
+        return self.work[1]
+
+    def __str__(self):
+        return str(self.work)
 
 
 def print_graph(work_graph):
@@ -10,19 +35,41 @@ def print_graph(work_graph):
             print edge,
         print
 
+
+def print_edges(edges):
+    for edge in edges:
+        print edge,
+    print
+
+
+def fuse(work_graph, edge):
+    # Узел нужно разделить, или склонировать
+    work_edge = copy.deepcopy(edge)
+
+    # Можно вливать узел
+    # Заменяем ссылки на себя
+    end_bands = work_graph[work_edge.end]
+    for band in end_bands:
+        # Передаем ссылки новому узлу
+        band.begin = work_edge.begin
+        work_graph[work_edge.begin].append(band)
+
+    # Пересчитываем связи и удаляем петли
+    for band in end_bands:
+        # Перенаправляем
+        edges = work_graph[band.end]
+        for edge in edges:
+            if edge.end == work_edge.end:
+                edge.work = (edge.begin, work_edge.begin)
+
+            if edge.begin == edge.end:
+                edges.remove(edge)
+
+    del work_graph[work_edge.end]  # в графе уже нет! ссылка локальна
+
+
 def main():
     source_graph = {1: [2, 4], 2: [1, 4, 3], 3: [2, 4], 4: [1, 2, 3]}
-
-    class Edge(object):
-        def __init__(self, begin, end):
-            # Исходное пложение ребра
-            self.source_ = (begin, end)
-
-            # Рабочее пложение ребра
-            self.work = (begin, end)
-
-        def __str__(self):
-            return str(self.work)
 
     # BAD! Но это указатели, а не копии объектов. Может не так все печально?
     work_graph = collections.defaultdict(list)
@@ -30,21 +77,16 @@ def main():
         for value in v:
             work_graph[k].append(Edge(k, value))
 
-
-
     # слить два узла
+    print_graph(work_graph)
     print 'fusing'
-    begin = 1
-    end = 2
-    begin_value = work_graph[begin]
-    end_value = work_graph[end]
-    del work_graph[end]  # в графе уже нет! ссылка локальна
 
-    # Можно вливать узел
+    edge = work_graph[1][0]
 
-    print end_value
-
+    fuse(work_graph, edge)
     print 'rest'
     print_graph(work_graph)
 
-main()
+
+if __name__ == '__main__':
+    main()
