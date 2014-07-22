20fcc52c747f460cacceaca1baef50a85210479d
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-07-22 19:48:58 +0400

    ref. try find bug.

--- src/stanford_algoritms_part1/graphs/scc.py
+++ src/stanford_algoritms_part1/graphs/scc.py
@@ -16,144 +16,98 @@ import sys
 import resource
 
 import gr_readers
+import util
 
 # TODO: BUGS - Iterative version is broken!!
 sys.setrecursionlimit(10 ** 6)
 resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, 2 ** 30))
 
-g_t = 0
-g_finals = {}
+
+class TraverseInfo(object):
+    def __init__(self):
+        self.g_t = 0
+        self.g_finals = {}
+        self.explored = set()
 
 
-def dfs_iterative_impl(G, SV, explored_set):
-    class Stack(object):
-        """ http://interactivepython.org/runestone/static/pythonds/BasicDS/stacks.html """
-        def __init__(self):
-            self.items = []
-
-        def empty(self):
-            return self.items == []
-
-        def push(self, item):
-            self.items.append(item)
-
-        def pop(self):
-            return self.items.pop()
-
-        def top(self):
-            return self.items[len(self.items) - 1]
-
-        def size(self):
-            return len(self.items)
+def dfs_iterative_impl(G, SV, tr):
     assert G
     assert SV
-    assert explored_set
+    assert tr
 
-    explored_set[SV] = True
-    S = Stack()
+    tr.explored.add(SV)
+    S = util.Stack()
     S.push(SV)
 
-    global g_t
-    copy_t = g_t
+    copy_t = tr.g_t
     while not S.empty():
         v = S.top()
         S.pop()
         for w in G[v]:
-            if not explored_set[w]:
-                explored_set[w] = True
+            if w not in tr.explored:
+                tr.explored.add(w)
                 S.push(w)
 
         # final
-        global g_finals
-        g_t += 1
-        g_finals[v] = g_t
-    return g_t - copy_t
+        # TODO: BUG is here somewhere
+        tr.g_t += 1
+        tr.g_finals[v] = tr.g_t
+    return tr.g_t - copy_t
 
 
-def dfs_separate_recursion_impl(G, SV, explored_set):
-    """ """
-    global g_t
-    copy_t = g_t
-
+def dfs_separate_recursion_impl(G, SV, tr):
     def __dfs(vertex):
-        explored_set[vertex] = True
+        tr.explored.add(vertex)
         ends = G[vertex]
         for w in ends:
-            if not explored_set[w]:
+            if w not in tr.explored:
                 __dfs(w)
 
-        global g_t, g_finals
-        g_t += 1
-        g_finals[vertex] = g_t
+        tr.g_t += 1
+        tr.g_finals[vertex] = tr.g_t
 
     assert G
     assert SV
-    assert explored_set
+    assert tr
 
+    copy_t = tr.g_t
     __dfs(SV)
-    return g_t - copy_t
+    return tr.g_t - copy_t
 
 
-def invert_digraph(g):
-    copy_gr = {}
-
-    for k in g.keys():
-        copy_gr[k] = []
-
-    for k, v in g.items():
-        ends = g[k]
-        for elem in ends:
-            copy_gr[elem].append(k)
-
-    return copy_gr
-
-
-def graph_rename(G, recoder):
-    gr_copy = {}
-    for k, v in G.items():
-        gr_copy[recoder[k]] = []
-        for elem in v:
-            gr_copy[recoder[k]].append(recoder[elem])
-
-    return gr_copy
-
-
-def scc(source_gr, dfs):
+def scc(source_gr, dfs, t):
     assert source_gr
     assert dfs
+    assert not t.explored
+    assert t.g_t == 0
 
     # Work
     RANGE = source_gr.keys()
 
     # inv.
-    gr_inv = invert_digraph(source_gr)
+    gr_inv = util.invert_digraph(source_gr)
 
     # Used DFS
-    explored_set = {}
-    for k, v in gr_inv.items():
-        explored_set[k] = False
+    for vertex in reversed(RANGE):
+        if vertex not in t.explored:
+            dfs(gr_inv, vertex, t)
 
-    for vertex in reversed(RANGE):  # TODO: bad. Ключи не обязательно следуют так.
-        if not explored_set[vertex]:
-            dfs(gr_inv, vertex, explored_set)
     # Next step
-    explored_set = {}
-    rename_gr = graph_rename(source_gr, g_finals)
+    t.explored = set()
+    rename_gr = util.graph_rename(source_gr, t.g_finals)
 
     tops = []
-    for k in gr_inv.keys():
-        explored_set[k] = False
-    for vertex in reversed(RANGE):  # TODO: bad. Ключи не обязательно следуют так.
-        if not explored_set[vertex]:
+    t.explored = set()
+    for vertex in reversed(RANGE):
+        if vertex not in t.explored:
             tops.append(vertex)
-            dfs(rename_gr, vertex, explored_set)
+            dfs(rename_gr, vertex, t)
 
     sizes = []
-    for k in gr_inv.keys():
-        explored_set[k] = False
-    for vertex in tops:  # TODO: bad. Ключи не обязательно следуют так.
-        if not explored_set[vertex]:
-            sizes.append(dfs(rename_gr, vertex, explored_set))
+    t.explored = set()
+    for vertex in tops:
+        if vertex not in t.explored:
+            sizes.append(dfs(rename_gr, vertex, t))
 
     sizes.extend([0, 0, 0, 0, 0])
     sizes.sort(reverse=True)
@@ -161,19 +115,20 @@ def scc(source_gr, dfs):
 
 
 def main():
-    # get_real_graph()#
-    #source_gr = get_fake_graph()
+    source_gr = gr_readers.get_fake_graph()
     source_gr = gr_readers.get_real_graph()
-    print "Readed. Start calc"
+
+    t = TraverseInfo()
+
     tops = scc(source_gr
-               , dfs_iterative_impl
-               #, dfs_separate_recursion_impl
+               #, dfs_iterative_impl
+               , dfs_separate_recursion_impl
                #, dfs_copy
+               , t
     )
     
     gold = [434821, 968, 459, 313, 211]
-    for a, b in zip(gold, tops[0:5]):
-        print a, b
+    #gold = [3, 3, 3, 0, 0]
     assert tops[0:5] == gold
 
 
---
+++ src/stanford_algoritms_part1/graphs/util.py
@@ -0,0 +1,44 @@
+__author__ = 'zaqwes'
+
+def invert_digraph(g):
+    copy_gr = {}
+
+    for k in g.keys():
+        copy_gr[k] = []
+
+    for k, v in g.items():
+        ends = g[k]
+        for elem in ends:
+            copy_gr[elem].append(k)
+
+    return copy_gr
+
+
+def graph_rename(G, recoder):
+    gr_copy = {}
+    for k, v in G.items():
+        gr_copy[recoder[k]] = []
+        for elem in v:
+            gr_copy[recoder[k]].append(recoder[elem])
+
+    return gr_copy
+
+class Stack(object):
+    """ http://interactivepython.org/runestone/static/pythonds/BasicDS/stacks.html """
+    def __init__(self):
+        self.items = []
+
+    def empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def top(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
