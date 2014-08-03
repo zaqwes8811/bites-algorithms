bf0926a664d6894538c78135b196d7f8f3f15f50
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-08-03 20:39:10 +0400

    save

--- src/stanford_algoritms_part1/w6_heap_hash/w6.cc
+++ src/stanford_algoritms_part1/w6_heap_hash/w6.cc
@@ -5,15 +5,33 @@
 
 using namespace std;
 
+template <typename T>
+class Heap {
+public:
+  // удаляет элемент - тут сразу проблема как со стеком
+  // setter не должен возвращать по значению
+  //T extract_min(); -> 
+  T top() const;
+  void pop();
+  
+  void insert(const T& val);
+  
+  void heapify(const vector<T>& unordered_data);
+  
+  // Вообще как это сделать? - нужно найти позицию
+  // http://stackoverflow.com/questions/2372994/search-an-element-in-a-heap
+  void delete_elem(const T& val);  // кажется нужно для алгоритма Дейкстры
+}
+
 int main() {
   int array[] = {10,20,30,5,15};
   vector<int> v(array, array+5);
   
-  // 
   for_each(begin(v), end(v), [] (const int& elem) { cout << elem << " "; });
   cout << endl;
   
   // http://stackoverflow.com/questions/7681779/easy-way-to-maintain-a-min-heap-with-stl
+  // по умолчанию less, и выходит max-heap
   make_heap(begin(v), end(v), greater<int>());  // min heap
   cout << v.front() << endl;  // TODO: top() - это первый элемент?
   
@@ -24,6 +42,9 @@ int main() {
   // Где вершина то?
   // Похоже нужно использовать один и тот же компаратор
   
+  /// Q1
+  // TODO: сделать все три варианта
   
+  /// Q2
   
 }
