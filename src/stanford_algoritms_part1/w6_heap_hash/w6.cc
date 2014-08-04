6d3af2830b111fdba47af30da177d60643b64e9f
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-08-04 12:27:59 +0400

    failed

--- src/stanford_algoritms_part1/w6_heap_hash/w6.cc
+++ src/stanford_algoritms_part1/w6_heap_hash/w6.cc
@@ -32,6 +32,7 @@ public:
 
 // Похоже для алгоритма Дектсры лучше bst 
 
+namespace fp {
 template <class OP1, class OP2>
 class compose_f_gx_t 
   : public std::unary_function<typename OP1::argument_type, typename OP2::result_type>
@@ -39,7 +40,18 @@ class compose_f_gx_t
   OP1 op1;
   OP2 op2;
 public:
-  
+  compose_f_gx_t(const OP1& o1, const OP2& o2) : op1(o1), op2(o2) {}
+  typename OP1::result_type
+  operator()(const typename OP2::argument_type& x) const {
+    return op1(op2(x));
+  }  
+};
+
+template <class OP1, class OP2>
+compose_f_gx(const OP1& o1, const OP2& o2) {
+  return compose_f_gx_t<OP1, OP2>(o1, o2);
+}
+
 }
 
 bool _2sum_naive(const vector<int>& in, const int target_sum)
@@ -49,7 +61,7 @@ bool _2sum_naive(const vector<int>& in, const int target_sum)
     vector<int>::const_iterator finded_it = 
       find_if(
 	in.begin(), in.end(), 
-	compose_f_gx(bind2nd(equal_to<int>(), target_sum), bind2nd(plus<int>(), *at)));
+	fp::compose_f_gx(bind2nd(equal_to<int>(), target_sum), bind2nd(plus<int>(), *at)));
     
     if (finded_it != in.end()) {
       finded = true;
