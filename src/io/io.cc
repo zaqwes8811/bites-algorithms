b5361887286a78784314d9cf456e3bdf03ec3f9a
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-08-14 18:35:11 +0400

    Extracting io.

--- src/io/io.cc
+++ src/io/io.cc
@@ -6,6 +6,27 @@
 
 using namespace std;
 
+// istream::sentry example
+#include <iostream>     // std::istream, std::cout
+#include <string>       // std::string
+#include <sstream>      // std::stringstream
+#include <locale>       // std::isspace, std::isdigit
+
+struct Phone {
+  std::string digits;
+};
+
+// custom extractor for objects of type Phone
+std::istream& operator>>(std::istream& is, Phone& tel)
+{
+    std::istream::sentry s(is);
+    if (s) while (is.good()) {
+      char c = is.get();
+      if (std::isspace(c,is.getloc())) break;
+      if (std::isdigit(c,is.getloc())) tel.digits += c;
+    }
+    return is;
+}
 
 int main() {
   string w1_1_filename = "../stanford_algoritms_part2/in_data/jobs.txt";
@@ -23,5 +44,11 @@ int main() {
     cout << line << endl;
   }
   
+  /// Extract objects
+  // http://www.cplusplus.com/reference/istream/istream/sentry/
+  std::stringstream parseme ("   (555)2326");
+  Phone myphone;
+  parseme >> myphone;
+  std::cout << "digits parsed: " << myphone.digits << '\n';
   return 0;
 }
