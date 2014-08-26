81389d305d227c41f6c8306961d16ba349c33865
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-08-26 16:51:44 +0400

    phy

--- src/stanford_algoritms_part2/sap2.pro
+++ src/stanford_algoritms_part2/sap2.pro
@@ -7,6 +7,7 @@ CONFIG -= qt
 # Libs
 BOOST=/home/zaqwes/work/third_party/boost_1_55_0
 DETAILS=..
+ADOBE=/home/zaqwes/work/third_party/source_release
 
 # но лучше бы 11 убрать, т.к. не ясно нужно ли доопределять некоторые функции
 # -std=c++11
@@ -19,11 +20,14 @@ SOURCES += \
   ../../3rdparty/gmock-1.6.0/fused-src/gmock-gtest-all.cc \
   w1_greedy.cc \
   w3_dyn_pro.cc \
+  w3_dyn_pro_parallel.cc \
+  w3_lession_code.cc \
   $$DETAILS/details/io_details.cc
 
 
 INCLUDEPATH += \
   $$BOOST $$DETAILS $$DETAILS/.. \
+  $$ADOBE \
   ../../3rdparty/gmock-1.6.0/fused-src/
 
 LIBS += -lpthread -lrt -ltbb
--- src/stanford_algoritms_part2/w3_dyn_pro.cc
+++ src/stanford_algoritms_part2/w3_dyn_pro.cc
@@ -1,3 +1,14 @@
+// Knapsack:
+//   - параллелизация не сильно помогла - в два раза только - хотя и вышло все просто
+//   - похоже нужен другой алгоритм 
+//
+// Если очень большой объем данных
+//   - Рекурсивная реализация
+//   - Использовать только 2 колонки - скорости это врядли прибавит
+//   - Образать веса которых не может быть - как в задаче из 1 части - похоже в алгоритме уже есть фильтр
+//   - Хэш таблица похоже безсмысленна, она реализована на основе массива - хотя...
+//   - Сортировка
+//
 #include <gtest/gtest.h>
 
 // C++98
@@ -12,17 +23,13 @@
 #include <boost/unordered_map.hpp>
 #include <google/dense_hash_map>
 #include <google/sparse_hash_map>
-
-#include "tbb/task_scheduler_init.h"
-#include "tbb/task.h"
-#include "tbb/concurrent_hash_map.h"
+#include <adobe/algorithm/sort.hpp>
 
 // inner
 #include "visuality/view.h"
 #include "details/io_details.h"
 
 using namespace std;
-using namespace tbb;
 
 using view::operator<<;
 using io_details::Item;
@@ -37,6 +44,12 @@ struct TaskId {
   int idx;
 };
 
+struct ItemsCompare {
+  bool operator()(const Item& lhs, const Item& rhs) {
+    return !(lhs.w < rhs.w);
+  }
+};
+
 struct KeyHash {
   std::size_t operator()(const TaskId& k) const
   {
@@ -49,13 +62,16 @@ struct KeyHash {
     //  http://stackoverflow.com/questions/2634690/good-hash-function-for-a-2d-index
     //  !!! http://web.archive.org/web/20071223173210/http://www.concentric.net/~Ttwang/tech/inthash.htm
     //
+    // Hash variants:
     //return boost::hash<pair<int, int> >()(make_pair(k.idx,k.w_bound));  // slow
-    return boost::hash<int>()(k.idx) ^ (boost::hash<int>()(k.w_bound) << 1);  // max speed
     //return boost::hash<int>()(k.w_bound) ^ (boost::hash<int>()(k.idx) << 1);
     //return boost::hash<int>()(k.idx) ^ (boost::hash<int>()(k.w_bound) >> 1);
     //return (size_t)((k.idx << 19) | (k.w_bound << 7));
     //return boost::hash<int>()(k.idx) * 37 + (boost::hash<int>()(k.w_bound));
     //return ((997 + boost::hash<int>()(k.idx)))*997 + boost::hash<int>()(k.w_bound);
+    
+    // max speed
+    return boost::hash<int>()(k.idx) ^ (boost::hash<int>()(k.w_bound) << 1);  
   }
 };
 
@@ -78,49 +94,15 @@ struct KeyEqual {
     return lhs.idx == rhs.idx && lhs.w_bound == rhs.w_bound;
   }
 };
-// Structure that defines hashing and comparison operations for user's type.
-struct TBBHashCompare {
-    static size_t hash( const TaskId& k ) {
-        return boost::hash<int>()(k.idx) ^ (boost::hash<int>()(k.w_bound) << 1);//KeyHash()(k);
-    }
-    //! True if strings are equal
-    static bool equal( const TaskId& lhs, const TaskId& rhs ) {
-        return lhs.idx == rhs.idx && lhs.w_bound == rhs.w_bound;//KeyEqual()(x,y);
-    }
-};
-
-typedef concurrent_hash_map<TaskId, int, TBBHashCompare> TaskTable;
-TaskTable g_task_table;
 
 ostream& operator<<(ostream& o, const TaskId& id) 
 {
   o << "(" << id.w_bound << ", " << id.idx << ")\n";
   return o;
 }
-    
-// Returns the maximum value that can be put in a knapsack of capacity W
-int knapSackExp(int w_bound, int n, const vector<Item>& items)
-{
-  // Base Case
-  if (n == 0 || w_bound == 0)
-      return 0;
+}  // namespace
 
-  // If weight of the nth item is more than Knapsack capacity W, then
-  // this item cannot be included in the optimal solution
-  cout << w_bound << " "<< n << endl;
-  if (items[n-1].w > w_bound)
-      return knapSackExp(w_bound, n-1, items);
-
-  // Return the maximum of two cases: (1) nth item included (2) not included
-  else {
-    int sum_values = std::max( 
-      knapSackExp(w_bound - items[n-1].w, n-1, items) + items[n-1].v, 
-      knapSackExp(w_bound,                n-1, items));
-
-    return sum_values;
-  }   
-}
-
+namespace {
 // Returns the maximum value that can be put in a knapsack of capacity W
 template <typename Store>
 int knapSack_hashtable(const TaskId& id, 
@@ -204,174 +186,6 @@ int knapSack_hashtable_2d(const TaskId& id,
     return sum_values;
   } 
 }
-
-// 
-int knapSack(int W, int wt[], int val[], int n)
-{
-   // Base Case
-   if (n == 0 || W == 0)
-       return 0;
- 
-   // If weight of the nth item is more than Knapsack capacity W, then
-   // this item cannot be included in the optimal solution
-   //cout << W << " "<< n << endl;
-   if (wt[n-1] > W)
-       return knapSack(W, wt, val, n-1);
- 
-   // Return the maximum of two cases: (1) nth item included (2) not included
-   else 
-     return std::max( 
-      knapSack(W - wt[n-1], wt, val, n-1) + val[n-1],
-      knapSack(W,           wt, val, n-1));
-}
-
-// DANGER: алгоритм пробуксовывает - иногда решает уже решенные задачи
-template <typename Store>
-class KnapsackTaskSerial : public task  {
-  Store* const store_;
-  const vector<Item>* items_;
-  int* const r_;
-  const TaskId id_;
-  
-  KnapsackTaskSerial(const KnapsackTaskSerial&);
-  KnapsackTaskSerial& operator=(const KnapsackTaskSerial&);
-  
-public:
-  KnapsackTaskSerial(const vector<Item>& items, Store& store, int& result, const TaskId& id) 
-    : store_(&store), items_(&items), r_(&result), id_(id)
-  {}
-  
-  task* execute()
-  { 
-    const bool connect = true;
-    // Похоже так лихо не выйдет - задача обязуется выполнить вычисления а сумма фейковая
-    // Если решена то вернуть результат
-    if (connect) {
-      TaskTable::const_accessor a;
-      if (store_->find(a, id_)) {
-        *r_ = a->second;
-        return NULL;
-      }
-    }
-    
-    // Некоторые задачи отбросили, но некоторые в процессе решения и будут готовы к моменту записи
-    {
-      // Work
-      int n = id_.idx;
-      int w_bound = id_.w_bound;
-      
-      // Base Case
-      if (n == 0 || w_bound == 0) {
-        *r_ = 0;  // можно сохранять, но на производительность сильно не влияет
-        return NULL;
-      }
-      
-      // Prepare
-      int delta = 0;
-      TaskId work_id(w_bound, n-1);
-      if (!((*items_)[n-1].w > w_bound)) {
-        work_id = TaskId(w_bound-(*items_)[n-1].w, n-1);
-        delta = (*items_)[n-1].v;
-      }
-      TaskId old_task(w_bound, n-1);
-
-      // Actions 
-      int x = 0;
-      int y = 0;
-      KnapsackTaskSerial& a = *new( allocate_child() ) KnapsackTaskSerial(*items_, *store_, x, work_id);
-      KnapsackTaskSerial& b = *new( allocate_child() ) KnapsackTaskSerial(*items_, *store_, y, old_task);
-      // Set ref_count to 'two children plus one for the wait".
-      set_ref_count(3);
-      // Start b running.
-      spawn( b );
-      // Start a running and wait for all children (a and b).
-      spawn_and_wait_for_all(a);
-      
-      x += delta;
-      *r_ = std::max(x, y);
-    }
-    
-    if (connect) {
-      // DANGER: похоже некоторые задачи таки решаются не один раз - и это уменьшает быстродействие
-      TaskTable::accessor a;
-      if (store_->insert(a, id_)) {
-        a->second = *r_;
-      } else {
-        // DANGER: промахов очень много
-        //cout << "BAD: Vain solved\n";
-      }
-    }
-    
-    return NULL;
-  }
-};
-}
-
-TEST(W3, Wis) 
-{
-  int arr[] = {0, 1, 4, 5, 4};  // 0 - guard
-  vector<int> W(arr, arr + sizeof(arr)/sizeof(arr[0]));
-  cout << W;
-
-  // Work
-  vector<int> A(W.size(), 0);
-  A[1] = W[1]; 
-  for (size_t i = 2; i < W.size(); ++i) {
-    // верширу либо добавляем либо
-    A[i] = std::max(A[i-1], A[i-2] + W[i]);
-    cout << A;
-  }
-  
-  // Reconstruction solution 
-  int i = W.size() - 1;
-  while (i >= 1) {
-    // Рассматриваем подзадачи, но сам вес из текущего узла
-    // Похоже на рассуждение при разработке алгоритма
-    if (A[i-1] >= A[i-2] + W[i]) {
-      // Веришина была исключена
-      --i;
-    } else {
-      // Веришина была включена, и нужно перескочить
-      cout << W[i] << " " << i << endl;
-      i -= 2;
-    }
-  }
-  
-}
-
-TEST(W3, KnapsackProblem) 
-{
-  pair<int, vector<Item > > tmp = get_test_items("NoFile");
-  vector<Item> items = tmp.second;
-  int W = tmp.first;
-  
-  
-  // A[0,0]
-  int N = items.size();
-  vector<vector<int> > A(N, vector<int>(W+1, 0)); 
-  for (int i = 1; i < N; ++i) {
-    
-    // Значения объема может быть любым с точностью до единицы
-    for (int x = 0; x <= W; ++x) {
-      int w_boundary = x - items[i].w;
-      int v = items[i].v;
-      if (w_boundary < 0) {
-        w_boundary = x;
-        v = 0;
-      }
-      
-      // Read
-      int invalid = A[i-1][x];
-      int valid = A[i-1][w_boundary];
-      
-      // Write
-      A[i][x] = std::max(invalid, valid + v);
-    }
-  }
-  
-  cout << " " << A;
-
-  // Reconstruction
 }
 
 TEST(W3, BigKnapsackProblem) 
@@ -380,13 +194,6 @@ TEST(W3, BigKnapsackProblem)
   // if w(i) > x 
   //   V(i, x) = V(i-1, x)
   //
-  //
-  // Очень большой объем данных
-  // - Рекурсивная реализация
-  // - Использовать только 2 колонки - скорости это врядли прибавит
-  // - Образать веса которых не может быть - как в задаче из 1 части - похоже в алгоритме уже есть фильтр
-  // - Хэш таблица похоже безсмысленна, она реализована на основе массива - хотя...
-  // - Сортировка
   pair<int, vector<Item > > tmp = get_test_items("NoFile");
   vector<Item> items = tmp.second;
   int W = tmp.first;
@@ -407,30 +214,12 @@ TEST(W3, BigKnapsackProblem)
       if (w_boundary < 0) {
         A[i][x] = A[i-1][x];  // просто копирование
       } else {
-        A[i][x] = std::max(A[i][x], A[i-1][w_boundary] + v);
+        A[i][x] = std::max(A[i][x], A[i-1][w_boundary] + v);  // BUG: looks like
       }
     }
   }
   
   cout << " " << A;
-}
-
-TEST(W3, GeeksForGeek) 
-{ 
-  pair<int, vector<Item > > tmp = get_test_items("NoFile");
-  vector<Item> items = tmp.second;
-  int W = tmp.first;
-
-  int count = items.size();
-  printf("sum(v(i)) = %d \n", knapSackExp(W, count, items));
-}
-
-TEST(W3, GeeksForGeekSrc) {
-    int val[] = {60, 100, 120};
-    int wt[] = {10, 20, 30};
-    int  W = 50;
-    int n = sizeof(val)/sizeof(val[0]);
-    printf("%d", knapSack(W, wt, val, n));
 }
 
 TEST(W3, GeeksForGeek_hashtable) 
@@ -454,6 +243,11 @@ TEST(W3, GeeksForGeek_hashtable_homework)
   
   ///*
   vector<Item> items = tmp.second;
+  
+  // Попробовать отсортировать по весу - только дольше
+  //adobe::sort(items);//, ItemsCompare());
+  //std::sort(items.begin(), items.end(), ItemsCompare());
+  
   int W = tmp.first;
 
   int count = items.size();
@@ -482,8 +276,8 @@ TEST(W3, GeeksForGeek_hashtable_homework)
 // Very slooow.
 TEST(W3, GeeksForGeek_hashtable_2homework) 
 { 
-  pair<int, vector<Item > > tmp = get_dyn_items("./input_data/knapsack1.txt");
-  //pair<int, vector<Item > > tmp = get_test_items("./input_data/knapsack_big.txt");
+  //pair<int, vector<Item > > tmp = get_dyn_items("./input_data/knapsack1.txt");
+  pair<int, vector<Item > > tmp = get_test_items("./input_data/knapsack_big.txt");
   
   ///*
   vector<Item> items = tmp.second;
@@ -499,41 +293,10 @@ TEST(W3, GeeksForGeek_hashtable_2homework)
   //assert(result == 4243395);
 }
 
-
-TEST(W3, ParallelKnap) {
-  pair<int, vector<Item > > tmp = 
-    get_dyn_items("./input_data/knapsack_big.txt");
-    //get_test_items("NoFile");
-  vector<Item> items = tmp.second;
-  int W = tmp.first;
-  int count = items.size();
-  int result = 0;
-  
-  TaskId root(W, count);
-  //int store = 0;
-  KnapsackTaskSerial<TaskTable>& a = *new(task::allocate_root()) 
-      KnapsackTaskSerial<TaskTable>(items, g_task_table, result, root);
-  task::spawn_root_and_wait(a);
-  //KnapsackTaskSerial<int>(items, store, result, root).execute();
-  
-  printf("sum(v(i)) = %d \n", result);
-  //assert(result == 8);
-  assert(result == 4243395);
-}
-
-
-TEST(W3, BottomUp) {
+TEST(W3, SparseColumn) {
   // TODO: похоже скорость должна быть много больше
 }
 
-
-// 
-TEST(W3, Genome) 
-{
+TEST(W3, BranchAndBound) {
   
-}
-
-TEST(W3, OptBST) 
-{
-  // Скорее всего тольков в теории
 }
