91104ec5ed1533618f65c1dd259c756708f74c52
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-09-15 23:25:52 +0400

    Add parallel part. It alg. part not only lang.

---
+++ parallel/cuda_nvidia_course/cs344/HW5/build/CMakeFiles/a.app.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/zaqwes/work/in-the-vicinity-cc/parallel/cuda_nvidia_course/cs344/HW5/reference_calc.cpp" "/home/zaqwes/work/in-the-vicinity-cc/parallel/cuda_nvidia_course/cs344/HW5/build/CMakeFiles/a.app.dir/reference_calc.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/cuda-5.5/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ parallel/cuda_nvidia_course/cs344/HW5/build/CMakeFiles/a.app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/a.app.dir/./a.app_generated_main.cu.o"
+  "CMakeFiles/a.app.dir/./a.app_generated_student.cu.o"
+  "CMakeFiles/a.app.dir/reference_calc.cpp.o"
+  "a.app.pdb"
+  "a.app"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/a.app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
