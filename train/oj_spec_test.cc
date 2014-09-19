dc15189f61f76a35f60b977fb4d137035ad1ad03
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-09-19 23:41:44 +0400

    Don't understood ideas. And how solve it.

--- train/oj_spec_test.cc
+++ train/oj_spec_test.cc
@@ -11,8 +11,13 @@ using namespace std;
 
 // O(n^2), нужно O(n)
 TEST(OJ, GasStantion) {
+  // Похоже на dp задачу - непрер. посл. длиной N и суммой >= 0 - нет, тут не то
+  //
+  // цикличность может что-то дать.
+  // 
   // если до конца не дойти, то вернуть -1
-  int gas_[] = {4, 1, 3, 6, 1}; vector<int> gas(gas_, gas_ + sizeof gas_ / sizeof gas_[0]);
+  int gas_[] = {4, 1, 3, 6, 1}; 
+  vector<int> gas(gas_, gas_ + sizeof gas_ / sizeof gas_[0]);
   int cost_[] = {2, 4, 2, 2, 3}; 
   vector<int> cost(cost_, cost_ + sizeof cost_ / sizeof cost_[0]);  // i -> i+1 
 
@@ -41,5 +46,28 @@ TEST(OJ, GasStantion) {
     rotate(gas.begin(), --gas.end(), gas.end());
     rotate(cost.begin(), --cost.end(), cost.end());
   }
+
+}
+
+// O(n) - почему 2N достаточно? не очивидно совсем
+// еще можно схлопнуть массивы
+TEST(OJ, GasStantionOn) {
+  // Похоже на dp задачу - непрер. посл. длиной N и суммой >= 0 - нет, тут не то
+  //
+  // цикличность может что-то дать.
+  // 
+  // если до конца не дойти, то вернуть -1
+  int gas_[] = {4, 1, 3, 6, 1}; 
+  vector<int> gas(gas_, gas_ + sizeof gas_ / sizeof gas_[0]);
+  int cost_[] = {2, 4, 2, 2, 3}; 
+  vector<int> cost(cost_, cost_ + sizeof cost_ / sizeof cost_[0]);  // i -> i+1 
+
+  vector<int> d(gas.size(), 0);  // d[i] - сколько нужно для перехода на gas[i+1]
+  for (int i = 0; i < gas.size(); ++i) {
+    d[i] = gas[i] - cost[i];
+  }
+
+  // 
+
 
 }
