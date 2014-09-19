d84fe4d91ac4a1b939bf3ba57aec90fdf976e5c5
zaqwes8811 <igor.a.lugansky@gmail.com>
2014-09-19 22:00:59 +0400

    Make O(n^2) but need On

--- train/oj_spec_test.cc
+++ train/oj_spec_test.cc
@@ -4,19 +4,42 @@
 
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace view;
 using namespace std;
 
+// O(n^2), нужно O(n)
 TEST(OJ, GasStantion) {
   // если до конца не дойти, то вернуть -1
   int gas_[] = {4, 1, 3, 6, 1}; vector<int> gas(gas_, gas_ + sizeof gas_ / sizeof gas_[0]);
-  int cost_[] = {2, 4, 2, 2, 3}; vector<int> cost(cost_, cost_ + sizeof cost_ / sizeof cost_[0]);
+  int cost_[] = {2, 4, 2, 2, 3}; 
+  vector<int> cost(cost_, cost_ + sizeof cost_ / sizeof cost_[0]);  // i -> i+1 
 
+  int idx = 0;
   for (int i = 0; i < gas.size(); ++i) {
-    int tank = 0;
+    int tank = gas.at(0);
+
+    idx = i;
+    for (int j = 1; j < gas.size(); ++j) {
+      tank -= cost[j-1];
+      if (tank < 0) {
+        idx = -1;
+        break;
+      }
+      tank += gas.at(j);
+    }
+
+    // последний переход
+    tank -= cost[gas.size()-1];
+    if (tank < 0) idx = -1;
+
+    if (tank >= 0)
+      break;
 
     // now rotate
+    rotate(gas.begin(), --gas.end(), gas.end());
+    rotate(cost.begin(), --cost.end(), cost.end());
   }
 
 }
