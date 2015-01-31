d1154f5ef56969c230561e38240bee93446fdeca
zaqwes <igor.a.lugansky@gmail.com>
2015-01-31 16:17:26 +0400

    clang build cilkplus failed

---
+++ source/test.c
@@ -0,0 +1,17 @@
+#include <cilk/cilk.h>
+#include <assert.h>
+
+int fib(int n) {
+  if (n < 2)
+    return n;
+  int a = cilk_spawn fib(n-1);
+  int b = fib(n-2);
+  cilk_sync;
+  return a + b;
+}
+
+int main() {
+  int result = fib(30);
+  assert(result == 832040);
+  return 0;
+} 
