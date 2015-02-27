540d818980a24a37b3c326560c3e2b84145b0f1f
zaqwes <igor.a.lugansky@gmail.com>
2015-02-27 16:58:45 +0300

    troubles with exception safty of LRU

--- projects/cc_algs_and_dss/tests/caching_lru_test.cc
+++ projects/cc_algs_and_dss/tests/caching_lru_test.cc
@@ -5,24 +5,60 @@
 #include <gtest/gtest.h>
 
 #include <functional>
+#include <map>  // small caches
+#include <list>
 
+// FIXME: how make exception safe
 template <typename K, typename V>
 class LRUCache {
 public:
   typedef std::function<V(K)> Operation;
 
-  explicit LRUCache(Operation op) : m_op(op) { }
+  explicit LRUCache(Operation op) :
+    m_op(op)
+    , k_maxSize(2) { }
 
-  V get(const K& key) {
-    return m_op(key);
+  // It's bad return from setter by value
+  //V
+  void get(const K& key, V& r_v) {
+    // if contain
+    if (m_store.find(key) != m_store.end())
+      r_v = m_store[key];
+
+    // not contain
+    V val = m_op(key);
+    m_store[key] = val;
+
+    try {
+      // if size to big remove
+      if (m_store.size() > k_maxSize) {
+        K steal = m_dll.front();
+        m_store.erase(steal);
+        m_dll.pop_front();
+      }
+    } catch(...) {
+      throw;
+    }
+
+    // just push
+    m_dll.push_back(key);
+
+    r_v = val;
   }
 
 private:
   Operation m_op;
+  const size_t k_maxSize;
 
   // DLL
+  std::list<K> m_dll;
 
   // Key-Value store
+  std::map<K, V> m_store;
+
+  // non-copyable
+  LRUCache(const LRUCache&);
+  LRUCache& operator=(const LRUCache&);
 };
 
 int get(int key) {
@@ -32,5 +68,6 @@ int get(int key) {
 TEST(DSS, Cache) {
   LRUCache<int, int> v(&get);
 
-  v.get(0);
+  int val = 0;
+  v.get(0, val);
 }
